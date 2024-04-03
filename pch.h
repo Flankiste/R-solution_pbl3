2236f1742c5f8ee01b9d150bf3d76765e80dba30
eliot marion <eliotmrn35@gmail.com>
2024-04-03 08:20:20 +0200

    tous les include

--- pch.h
+++ pch.h
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "CLParcours.h"
+#include "CLparcours2D.h"
+#include "CLparcours3D.h"
+#include "CLpoint.h"
+#include "CLpoint3D.h"
 
 #endif
 using namespace std;
